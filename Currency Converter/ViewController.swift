cba971b2d0a26244ef842485d2516fb6f7efad77
MohamedAlsadek <MohamedAlsadek@s.local>
2015-10-31 19:01:52 +0200

    handle scroll view and test it iPhone 6

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -8,26 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MVSelectorScrollViewDelegate {
 
     @IBOutlet weak var textFieldNumber: UITextField!
+    @IBOutlet weak var scrollViewCurrencies: MVSelectorScrollView!
+    @IBOutlet var viewParent: UIView!
+    
     let currencyFormatter = NSNumberFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        viewParent.hidden = true
         getCurrencyDataFromServer ()
-        initUIElements()
+        
     }
     
-    func initUIElements () {
-        handleTextFieldFormatting()
-    }
-    
+
+    // MARK: Get Data
     func getCurrencyDataFromServer () {
         CurrencyParser.parseCurrency { (isFinished) -> Void in
             if isFinished {
+                self.viewParent.hidden = false
+                self.initUIElements()
                 print("succefully get the data.")
             }else {
                 Utilities.displayErrorFromView(self, message: "Error in connection")
@@ -35,11 +38,12 @@ class ViewController: UIViewController {
         }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    // MARK: Init UI
+    func initUIElements () {
+        handleTextFieldFormatting()
+        initScrollViewCurrencies ()
+    }
     
     // MARK: UITextField
     func handleTextFieldFormatting () {
@@ -54,6 +58,26 @@ class ViewController: UIViewController {
     func textFieldDidChange(textField: UITextField) {
         let text = textField.text!.stringByReplacingOccurrencesOfString(currencyFormatter.currencySymbol, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.groupingSeparator, withString: "").stringByReplacingOccurrencesOfString(currencyFormatter.decimalSeparator, withString: "")
         textField.text = currencyFormatter.stringFromNumber((text as NSString).doubleValue / 100.0)
+    }
+    
+    // MARK: UIScrollView
+    func initScrollViewCurrencies () {
+        
+        let scrollViewValues = ["USD" , "GBP" , "EUR" , "CAD" , "JPY"]
+        scrollViewCurrencies.values = scrollViewValues
+        scrollViewCurrencies.delegate = self
+        scrollViewCurrencies.updateIndexWhileScrolling = false;
+    }
+    
+    func scrollView(scrollView: MVSelectorScrollView!, pageSelected: Int) {
+        print(scrollView.values[pageSelected])
+    }
+    
+    
+    // MARK: MemoryWarning
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
 }
